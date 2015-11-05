ec151364f2c17acc3d51fbdb397d87634432681c
DrakeCannan <DrakeCannan@gmail.com>
2015-11-05 15:57:07 -0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+#Tested on java version 1.8.0_65
+#Java(TM) SE Runtime Environment (build 1.8.0_65-b17)
+#Java HotSpot(TM) 64-Bit Server VM (build 25.65-b01, mixed mode)
+
+#Tested using libraries from Java Development Kit 8 8u65
+
+# Compile java programs
+javac ./src/tweets_cleaned.java
+javac ./src/average_degree.java
+
+# Execute java programs
+java tweets_cleaned
+java average_degree
